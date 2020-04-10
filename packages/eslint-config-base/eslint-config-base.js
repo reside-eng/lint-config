9d33552856604b2d0a867ddccd37ef4560edf3a7
Eddie <hershmire@users.noreply.github.com>
2020-04-10 16:30:13 -0600

    feat: add some additional rules we need for Side (#5)
    Co-authored-by: Eddie Leffler <757524+hershmire@users.noreply.github.com>

--- packages/eslint-config-base/eslint-config-base.js
+++ packages/eslint-config-base/eslint-config-base.js
@@ -1,3 +1,44 @@
 module.exports = {
   extends: ['airbnb-base'],
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          // From Airbnb:
+          // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/imports.js#L71
+          //
+          // We want to still allow everything that Airbnb set as default but
+          // also want to provide some additional items specific to Side's needs
+          'test/**', // tape, common npm pattern
+          'tests/**', // also common npm pattern
+          'spec/**', // mocha, rspec-like pattern
+          '**/__tests__/**', // jest pattern
+          '**/__mocks__/**', // jest pattern
+          'test.{js,jsx}', // repos with a single test file
+          'test-*.{js,jsx}', // repos with multiple top-level test files
+          '**/*{.,_}{test,spec}.{js,jsx}', // tests where the extension or filename suffix denotes that it is a test
+          '**/jest.config.js', // jest config
+          '**/jest.setup.js', // jest setup
+          '**/vue.config.js', // vue-cli config
+          '**/webpack.config.js', // webpack config
+          '**/webpack.config.*.js', // webpack config
+          '**/rollup.config.js', // rollup config
+          '**/rollup.config.*.js', // rollup config
+          '**/gulpfile.js', // gulp config
+          '**/gulpfile.*.js', // gulp config
+          '**/Gruntfile{,.js}', // grunt config
+          '**/protractor.conf.js', // protractor config
+          '**/protractor.conf.*.js', // protractor config
+          '**/karma.conf.js', // karma config
+          //
+          // Side's custom addition
+          'scripts/**/*',
+          'babel.config.js',
+          '**/*.stories.js',
+        ],
+        optionalDependencies: false,
+      },
+    ],
+  },
 };
--- packages/eslint-config-react/eslint-config-react.js
+++ packages/eslint-config-react/eslint-config-react.js
@@ -1,3 +1,15 @@
 module.exports = {
   extends: ['@side-eng/eslint-config-base', 'airbnb'],
+  rules: {
+    // AirBnB is using .jsx, we want to also allow .js
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.js'] }],
+    'react/jsx-handler-names': [
+      'error',
+      {
+        // AirBnB is disabling this rule by default
+        eventHandlerPrefix: 'handle',
+        eventHandlerPropPrefix: 'on',
+      },
+    ],
+  },
 };
