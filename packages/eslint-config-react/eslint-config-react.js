00b00a58fbc349bae9c2199e78f70c44bb2f0d5d
Eddie <hershmire@users.noreply.github.com>
2020-05-05 15:52:24 -0600

    fix: provide core settings for base rules (#9)
    Allow consumer to just get the barebone set of rules for eslint-config-base so they can properly extend other possible conflicing configs in the correct order.
    
    Co-authored-by: Eddie Leffler <757524+hershmire@users.noreply.github.com>

--- packages/eslint-config-react/eslint-config-react.js
+++ packages/eslint-config-react/eslint-config-react.js
@@ -1,7 +1,11 @@
 module.exports = {
   extends: [
     'eslint-config-airbnb',
-    '@side-eng/eslint-config-base',
+    // We only want @side-eng/eslint-config-base/core (rules only) so we can
+    // properly override eslint-config-airbnb (which already includes
+    // eslint-config-airbnb-base) without blowing away some of their settings we
+    // still want.
+    '@side-eng/eslint-config-base/core',
     '@side-eng/eslint-config-prettier/react',
     './rules/react',
   ].map(require.resolve),
