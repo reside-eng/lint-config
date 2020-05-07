4c06e8b5bded6c803cdf73b7ef52e92466530271
Eddie Leffler <757524+hershmire@users.noreply.github.com>
2020-05-07 13:52:03 -0600

    feat: add jest linting to base config

--- packages/eslint-config-base/eslint-config-base.js
+++ packages/eslint-config-base/eslint-config-base.js
@@ -5,4 +5,27 @@ module.exports = {
     './core',
   ].map(require.resolve),
   rules: {},
+  overrides: [
+    {
+      extends: ['plugin:jest/all'],
+      files: [
+        '**/__tests__/**',
+        '**/__mocks__/**',
+        'test.{js,jsx}',
+        'test-*.{js,jsx}',
+        '**/*{.,_}{test,spec}.{js,jsx}',
+      ],
+      rules: {
+        'jest/prefer-expect-assertions': 0,
+        'jest/lowercase-name': [
+          'error',
+          {
+            ignore: ['describe'],
+          },
+        ],
+        'jest/no-disabled-tests': 0,
+        'jest/no-hooks': 0,
+      },
+    },
+  ],
 };
