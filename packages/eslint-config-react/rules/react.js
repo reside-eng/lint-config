614d52e149596dae00d05aba47ec35168b27e71c
Eddie Leffler <757524+hershmire@users.noreply.github.com>
2020-05-15 11:38:49 -0600

    fix: ignore default props for functional components

--- packages/eslint-config-react/rules/react.js
+++ packages/eslint-config-react/rules/react.js
@@ -8,5 +8,14 @@ module.exports = {
         eventHandlerPropPrefix: 'on',
       },
     ],
+    'react/require-default-props': [
+      'error',
+      {
+        // TODO: Modify this once support for checking functional component's
+        // arguments for default params is supported.
+        // https://github.com/yannickcr/eslint-plugin-react/issues/2396
+        ignoreFunctionalComponents: true,
+      },
+    ],
   },
 };
