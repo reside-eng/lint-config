de300a44e190bafede950ef3bac0c4961d1d175e
Eddie <hershmire@users.noreply.github.com>
2020-06-30 10:53:38 -0600

    feat: typescript test extentions for import/no-extraneous-dependencies (#23)
    Co-authored-by: Eddie Leffler <757524+hershmire@users.noreply.github.com>

--- packages/eslint-config-base/rules/imports.js
+++ packages/eslint-config-base/rules/imports.js
@@ -85,6 +85,11 @@ module.exports = {
           '**/babel.config.js',
           '**/*.stories.{js,jsx}',
           '**/.eslintrc.js',
+          //
+          // Support for TypeScript extensions
+          'test.{ts,tsx}', // repos with a single test file
+          'test-*.{ts,tsx}', // repos with multiple top-level test files
+          '**/*{.,_}{test,spec}.{ts,tsx}', // tests where the extension or filename suffix denotes that it is a test
         ],
       },
     ],
