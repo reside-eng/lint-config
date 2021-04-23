91f93b41013eb3ecea3d369a6abf5801f6534fc6
Jonathan May <jonathan.may@sideinc.com>
2021-04-23 12:09:09 -0700

    fix: update extends for eslint-config-prettier v8 changes (#134)
    * fix: update extends for eslint-config-prettier v8 changes
    
    * refactor: remove line break

--- packages/eslint-config-prettier/react.js
+++ packages/eslint-config-prettier/react.js
@@ -1,9 +1,7 @@
 module.exports = {
   extends: [
     'plugin:prettier/recommended',
-    ...['eslint-config-prettier/react', './rules/prettier'].map(
-      require.resolve,
-    ),
+    ...['eslint-config-prettier', './rules/prettier'].map(require.resolve),
   ],
   rules: {},
 };
