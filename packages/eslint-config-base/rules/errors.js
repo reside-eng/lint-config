15045bf910563638b6615dbbaeb5cf01afe35078
Zach Tindall <zach.tindall@sideinc.com>
2022-01-20 14:55:20 -0500

    fix: turn off class-methods-use-this rule

--- packages/eslint-config-base/rules/errors.js
+++ packages/eslint-config-base/rules/errors.js
@@ -1,4 +1,11 @@
 module.exports = {
+  rules: {
+    /**
+     * Turn off class methods use this
+     * @link https://github.com/reside-eng/guidelines/blob/main/rfcs/shared-patterns/typescript/002-class-methods-use-this.md
+     */
+    'class-methods-use-this': 0,
+  },
   overrides: [
     {
       files: ['bin/**', 'scripts/**'],
