e82f45dfe27b499595ac82c6b813a3ccf83d3a2b
Hunter Tunnicliff <hunter.tunnicliff@sideinc.com>
2022-03-25 11:43:56 -0700

    fix: disable `arrow-body-style` lint rule

--- packages/eslint-config-base/rules/errors.js
+++ packages/eslint-config-base/rules/errors.js
@@ -5,6 +5,21 @@ module.exports = {
      * @link https://github.com/reside-eng/guidelines/blob/main/rfcs/shared-patterns/typescript/002-class-methods-use-this.md
      */
     'class-methods-use-this': 0,
+    /**
+     * Both forms of arrow functions are allowed.
+     *
+     * {@link https://residenetwork.atlassian.net/wiki/spaces/PLAT/pages/2570780871/UI+Working+Group+Meetings UI WG meeting decision on March 25, 2022}
+     *
+     * @example
+     * ```ts
+     * const foo = () => {
+     *   return 'bar';
+     * };
+     *
+     * const foo = () => 'bar';
+     * ```
+     */
+    'arrow-body-style': 0,
   },
   overrides: [
     {
