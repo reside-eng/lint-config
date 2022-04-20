7757e37c641e06556faf023b2351bede7c80f7e8
Hunter Tunnicliff <hunter.tunnicliff@sideinc.com>
2022-04-20 10:27:06 -0700

    feat(jest): move rules to test file override
    BREAKING CHANGES: Rules are applied using an override.

---
+++ packages/eslint-config-jest/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  overrides: [
+    {
+      files: [
+        '**/__tests__/**',
+        '**/__mocks__/**',
+        '*.test.*',
+        '*.spec.*',
+        'jest.setup.{ts,js}',
+      ],
+      excludedFiles: ['cypress/**/*'],
+      extends: ['plugin:jest/recommended', 'plugin:jest/style'],
+      rules: {
+        'jest/prefer-lowercase-title': ['error', { ignore: ['describe'] }],
+        'jest/no-disabled-tests': 0,
+      },
+    },
+    {
+      files: ['jest.setup.*'],
+      rules: {
+        'jest/require-top-level-describe': 0,
+        'import/no-extraneous-dependencies': 0,
+      },
+    },
+  ],
+};
