ffe13a7363590cf1cbadb66c11a19ab6294c166c
Hunter Tunnicliff <hunter.tunnicliff@sideinc.com>
2022-04-21 10:48:30 -0700

    feat(base): configure `no-unused-vars` rule

--- packages/eslint-config-typescript/rules/typescript-eslint.js
+++ packages/eslint-config-typescript/rules/typescript-eslint.js
@@ -3,6 +3,7 @@ module.exports = {
     {
       files: ['**/*.ts?(x)'],
       rules: {
+        'no-unused-vars': 'off',
         // Allow unused variables only when they are prefixed with an underscore (e.g. _foo)
         '@typescript-eslint/no-unused-vars': [
           'error',
