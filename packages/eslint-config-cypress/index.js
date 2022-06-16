2f4dbc5296848843525ff7ad4af2881d4b6bae03
Hunter Tunnicliff <hunter.tunnicliff@sideinc.com>
2022-06-16 11:50:50 -0700

    feat: add overrides for new cypress config files [PLAT-2348]

--- packages/eslint-config-cypress/index.js
+++ packages/eslint-config-cypress/index.js
@@ -11,5 +11,14 @@ module.exports = {
         'cypress/no-force': 'error',
       },
     },
+    {
+      files: ['cypress.config.*'],
+      rules: {
+        // Cypress uses default export
+        'import/no-default-export': 'off',
+        // Cypress is allowed to use devDependencies
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
   ],
 };
