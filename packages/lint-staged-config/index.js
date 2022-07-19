dd4373f911cce65daf9e19bb2545a8c6b14c2774
Zach Tindall <a88zach@users.noreply.github.com>
2022-07-19 13:12:52 -0400

    feat: add shared lint staged config (#386)
    * feat: add shared lint staged config
    
    * fix: add additional peer dep
    
    * fix: update lint staged to only run prettier on staged files
    
    * fix: only format staged files
    
    * fix: add html and css
    
    * fix: update readme to use a different file name for lint staged

---
+++ packages/lint-staged-config/index.js
@@ -0,0 +1,23 @@
+const { ESLint } = require('eslint');
+
+const removeIgnoredFiles = async (files) => {
+  const eslint = new ESLint();
+  const isIgnored = await Promise.all(
+    files.map((file) => eslint.isPathIgnored(file)),
+  );
+  const filteredFiles = files.filter((_, i) => !isIgnored[i]);
+  return filteredFiles.join(' ');
+};
+
+module.exports = {
+  '*.{ts,tsx,js,jsx}': async (files) => {
+    const filesToLint = await removeIgnoredFiles(files);
+    return [
+      `eslint --max-warnings=0 --fix ${filesToLint}`,
+      `prettier --write ${files.join(' ')}`,
+    ];
+  },
+  '*.{yaml,yml,json,md,html,css}': (files) => [
+    `prettier --write ${files.join(' ')}`,
+  ],
+};
