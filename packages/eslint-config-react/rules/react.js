45900d7963b29be516f36d680fd436f11b7f522d
Mark Battersby <mark.battersby@sideinc.com>
2022-09-13 19:33:54 -0400

    feat: remove react/require-default-props
    Per the vote on https://github.com/reside-eng/guidelines/discussions/25

--- packages/eslint-config-react/rules/react.js
+++ packages/eslint-config-react/rules/react.js
@@ -8,15 +8,6 @@ module.exports = {
         eventHandlerPropPrefix: 'on',
       },
     ],
-    'react/require-default-props': [
-      'error',
-      {
-        // TODO: Modify this once support for checking functional component's
-        // arguments for default params is supported.
-        // https://github.com/yannickcr/eslint-plugin-react/issues/2396
-        ignoreFunctionalComponents: true,
-      },
-    ],
     // Next.JS does not require JSX to be in scope
     'react/react-in-jsx-scope': 'off',
     // We're okay with this but use it sparingly and only when this is intentional
