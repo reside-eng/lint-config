79e1b70c95dd01439c3068d989af84308fdb2a06
Scott Prue <scott@sideinc.com>
2023-11-14 15:57:07 -0700

    fix(next): allow underscore dangle for typename

--- packages/eslint-config-next/rules/graphql.js
+++ packages/eslint-config-next/rules/graphql.js
@@ -4,5 +4,17 @@ module.exports = {
       files: ['*.graphql'],
       extends: 'plugin:@graphql-eslint/operations-recommended',
     },
+    {
+      files: ['**/*.{ts,tsx}'],
+      extends: ['plugin:@typescript-eslint/recommended'],
+      rules: {
+        'no-underscore-dangle': [
+          'error',
+          {
+            allow: ['__typename'],
+          },
+        ],
+      },
+    },
   ],
 };
