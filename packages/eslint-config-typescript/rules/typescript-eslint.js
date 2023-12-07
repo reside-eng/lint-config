b5f032244d769e6e724c116b681f18ac2dec75a7
Hunter Tunnicliff <hunter.tunnicliff@sideinc.com>
2023-12-07 11:41:14 -0800

    fix: correct overwritten rules (#595)

--- packages/eslint-config-typescript/rules/typescript-eslint.js
+++ packages/eslint-config-typescript/rules/typescript-eslint.js
@@ -2,6 +2,15 @@ module.exports = {
   overrides: [
     {
       files: ['**/*.ts?(x)'],
+      extends: [
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        tsconfigRootDir: process.cwd(),
+        project: true,
+      },
       rules: {
         'no-unused-vars': 'off',
         // Allow unused variables only when they are prefixed with an underscore (e.g. _foo)
