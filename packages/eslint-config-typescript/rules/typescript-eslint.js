a27be9e88c661114e23e39a0b9e7e17c5af54755
Scott Prue <scott@sideinc.com>
2024-01-16 12:43:09 -0700

    feat(typescript): force type imports

--- packages/eslint-config-typescript/rules/typescript-eslint.js
+++ packages/eslint-config-typescript/rules/typescript-eslint.js
@@ -22,7 +22,13 @@ module.exports = {
             caughtErrorsIgnorePattern: '^_',
           },
         ],
-        '@typescript-eslint/consistent-type-definitions': 'off',
+        // Force imports to use type (not included in code transpiled by tsc to commonjs)
+        '@typescript-eslint/consistent-type-imports': [
+          'error',
+          { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
+        ],
+        // NOTE: prefer-top-level causes duplicate imports for types
+        'import/no-duplicates': ['error', { 'prefer-inline': true }],
       },
     },
   ],
