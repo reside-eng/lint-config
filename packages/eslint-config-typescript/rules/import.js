907f24970c89a5a564e7a7eb304ae24dc090fbdb
Scott Prue <scott@sideinc.com>
2024-05-30 14:07:44 -0600

    fix: run formatting

--- packages/eslint-config-typescript/rules/import.js
+++ packages/eslint-config-typescript/rules/import.js
@@ -3,10 +3,29 @@ module.exports = {
     'import/parsers': {
       [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
     },
-    'import/extensions': ['.js', '.jsx', '.mjs', '.ts', '.tsx', '.cjs', '.mts', '.cts'],
+    'import/extensions': [
+      '.js',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.cjs',
+      '.mts',
+      '.cts',
+    ],
     'import/resolver': {
       node: {
-        extensions: ['.js', '.jsx', '.mjs', '.json', '.ts', '.tsx', '.cjs', '.mts', '.cts'],
+        extensions: [
+          '.js',
+          '.jsx',
+          '.mjs',
+          '.json',
+          '.ts',
+          '.tsx',
+          '.cjs',
+          '.mts',
+          '.cts',
+        ],
       },
       typescript: {
         alwaysTryTypes: true,
@@ -25,7 +44,7 @@ module.exports = {
         tsx: 'never',
         cjs: 'never',
         mts: 'never',
-        cts: 'never'
+        cts: 'never',
       },
     ],
   },
